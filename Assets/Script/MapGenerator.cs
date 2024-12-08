df4cba379e89d80af6afe3a9a5ba55b52e9b3817
ShiomiKazuma <kazu_shio_1217@icloud.com>
2024-12-08 19:45:46 +0900

    [fix]マップ生成機能の修正

--- Assets/Script/MapGenerator.cs
+++ Assets/Script/MapGenerator.cs
@@ -1,13 +1,19 @@
 using System;
+using Alchemy.Inspector;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
 public class MapGenerator : MonoBehaviour
 {
+    [LabelText("マスの間隔")]
     [SerializeField] private float _interval = 2f;
+    [LabelText("普通の地面")]
     [SerializeField] private GameObject _normalGroundPrefab;
+    [LabelText("水のフィールド")]
     [SerializeField] private GameObject _waterFieldPrefab;
+    [LabelText("マップの縦")]
     [SerializeField] private int _height = 15;
+    [LabelText("マップの幅")]
     [SerializeField] private int _width = 15;
     private GameObject _mapParent;
 
